6f6fcaa38e35a5502a3b9c6e318d57e5e4fdb715
Igormcf <igormaciel82@gmail.com>
2022-08-01 17:17:34 -0300

    Finaliza requisito 07

---
+++ src/middlewares/validUser.ts
@@ -0,0 +1,24 @@
+import Joi from 'joi';
+import { Request, Response, NextFunction } from 'express';
+
+const userDTO = Joi.object({
+  username: Joi.string().min(3).required(),
+  classe: Joi.string().min(3).required(),
+  level: Joi.number().min(1).required(),
+  password: Joi.string().min(8).required(),
+});
+
+const validUser = (req: Request, res: Response, next: NextFunction) => {
+  const { error } = userDTO.validate(req.body);
+
+  if (error) {
+    const [message] = error.details.map((e) => e.message);
+    console.log(error.details[0].message, 'error Joi Product');
+    return error.details[0].message.includes('required') ? res.status(400).json({ message })
+      : res.status(422).json({ message });
+  }
+
+  return next();
+};
+
+export default validUser;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import UsersController from '../controllers/users.controller';
 
+import validUser from '../middlewares/validUser';
+
 const router = Router();
 
 const userController = new UsersController();
 
-router.post('/users', userController.createUser);
+router.post('/users', validUser, userController.createUser);
 
 export default router;
